56a3e7ec41dde0c1773cfa96e79fdf7fb6c5394b
Zero Hero <0xzerohero@gmail.com>
2025-07-23 18:11:48 +0300

    Set solana vertion to 2.2

--- jito_protos/src/convert.rs
+++ jito_protos/src/convert.rs
@@ -33,7 +33,7 @@ pub fn packet_to_proto_packet(p: &Packet) -> Option<ProtoPacket> {
                 forwarded: p.meta().forwarded(),
                 repair: p.meta().repair(),
                 simple_vote_tx: p.meta().is_simple_vote_tx(),
-                tracer_packet: p.meta().is_tracer_packet(),
+                tracer_packet: false,
                 from_staked_node: p.meta().is_from_staked_node(),
             }),
             sender_stake: 0,
@@ -68,9 +68,6 @@ pub fn proto_packet_to_packet(p: &ProtoPacket) -> Packet {
             }
             if flags.forwarded {
                 packet.meta_mut().flags.insert(PacketFlags::FORWARDED);
-            }
-            if flags.tracer_packet {
-                packet.meta_mut().flags.insert(PacketFlags::TRACER_PACKET);
             }
             if flags.repair {
                 packet.meta_mut().flags.insert(PacketFlags::REPAIR);
